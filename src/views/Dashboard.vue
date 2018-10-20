cc82f52188270c13b25c24eb229b375252fcc1c1
tiberiu.chiritoiu <chutubi@yahoo.com>
2018-10-20 13:22:10 +0300

    response done

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,28 +1,43 @@
 <template>
 <div class="">
-	<v-layout row wrap>
-		<v-data-iterator :items="items" :pagination.sync="pagination" :total-items="total" :rows-per-page-items="rppi">
-
-			<template slot="item" slot-scope="props">
-				<v-layout row wrap justify-space-around>
-					<div class="">
-						{{item.name}}
-					</div>
-					<div class="">
-						{{user.surname}}
-					</div>
-					<div class="">
-						{{user.gender}}
-					</div>
-					<div class="">
-						{{user.region}}
-					</div>
-				</v-layout>
-			</template>
-
-
-		</v-data-iterator>
-	</v-layout>
+	<v-card tile class="my-2 py-2">
+		<v-layout row wrap>
+			<v-flex xs3 class="px-2">
+				Name
+			</v-flex>
+			<v-flex xs3 class="px-2">
+				Surname
+			</v-flex>
+			<v-flex xs3>
+				Gender
+			</v-flex>
+			<v-flex xs3>
+				Region
+			</v-flex>
+		</v-layout>
+	</v-card>
+	<v-card>
+		<v-layout row wrap>
+			<v-data-iterator :items="items" :pagination.sync="pagination" :total-items="total" :rows-per-page-items="rppi" style="width: 100%">
+				<template slot="item" slot-scope="props">
+					<v-layout row wrap>
+						<v-flex xs3>
+							{{props.item.name}}
+						</v-flex>
+						<v-flex xs3>
+							{{props.item.surname}}
+						</v-flex>
+						<v-flex xs3>
+							{{props.item.gender}}
+						</v-flex>
+						<v-flex xs3>
+							{{props.item.region}}
+						</v-flex>
+					</v-layout>
+				</template>
+			</v-data-iterator>
+		</v-layout>
+	</v-card>
 </div>
 </template>
 <script>
@@ -43,11 +58,10 @@ export default {
 		getUsers() {
 			// setup query endpoint
 			let endpoint = 'http://uinames.com/api/?amount=25';
-			console.log("endpoint", endpoint);
+
 			this.$axios.get(endpoint)
 				.then(response => {
-					this.items = response;
-					console.log("user", response);
+					this.items = response.data;
 				})
 				.catch(error => {
 					console.log(error);
